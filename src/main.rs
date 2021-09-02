e59f2293415482b9d268ae820934d858bb357678
Aditya Badrinath <abadrinath15@gmail.com>
2021-09-02 00:08:13 -0400

    Some more functions to trait up, some more comments.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+//! Trying to learn Rust by building a simple exchange as discussed in a Jane Street video!
+#![warn(missing_docs)]
 pub mod order_infra;
 
 fn main() {
--- src/order_infra/order_book.rs
+++ src/order_infra/order_book.rs
@@ -4,6 +4,13 @@ use super::order;
 struct OrderNotInBookError {}
 
 trait SecurityOrderBook<T> {
-    fn add_order(new_order: order::SingleOrder) -> T;
-    //fn cancel_order(order_ref: T) -> Result((), )
+    /// Adds an order to the book and returns an order ID which can retrieve the order from the same
+    /// book.
+    /// # Examples
+    fn add_order(&mut self, new_order: &order::SingleOrder) -> T;
+
+    /// Removes an order from the book as identified by an order number, returning the order if it
+    /// is found in the book or an error if not found.
+    /// # Examples
+    fn remove_order(&mut self, order_number: T) -> Result<order::SingleOrder, OrderNotInBookError>;
 }
